98166f8fc6e4f7ba168f4fb23a3a781978def468
Noor <nkilano@hotmail.com>
2021-09-29 17:14:50 +0200

    Update B-1.js

--- B-1.js
+++ B-1.js
@@ -2,8 +2,8 @@
 //Write a program using JavaScript that will print all the numbers from 1 to 135.
 
 // pseudo code 
-// first we will create tow variables the first one content the (1) the second one content 
-// the result of adding the numbers then we print the result.
+ //first we will create for loop the content let  (x=1) then compere x if small or equal 135
+// then add 1 to x and print the value of x then do  the process again and again until 135
 //diagram  
 // 
 
