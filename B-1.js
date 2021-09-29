2a1680b7c2e9e23bd1f9f997b36b48014a47ab1e
Noor <nkilano@hotmail.com>
2021-09-29 16:43:11 +0200

    add B-1.html via uplod

---
+++ B-1.js
@@ -0,0 +1,14 @@
+//Print 1 - 135
+//Write a program using JavaScript that will print all the numbers from 1 to 135.
+
+// pseudo code 
+// first we will create tow variables the first one content the (1) the second one content 
+// the result of adding the numbers then we print the result.
+//diagram  
+// 
+
+//code
+
+for (let x =1; x <= 135; x++) {
+    console. log(x)
+}
