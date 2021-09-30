37e0fc3145dfdd8f4f96d466473a95ec7c2b3c78
Noor <nkilano@hotmail.com>
2021-09-30 22:21:48 +0200

    Add files via upload

---
+++ B-4.js
@@ -0,0 +1,12 @@
+//4. Print the elements of an array
+//Given an array X = [1,4,2,12], write a program that will iterate through the array and
+// print all numbers on the screen. 
+//Your program should work with arrays of all sizes.
+
+// pseudo code 
+// first we will create a variable num is array.
+// then console. log result. 
+
+//code
+const num =[1,4,2,12,'rrr',144,16];
+console. log (num)
