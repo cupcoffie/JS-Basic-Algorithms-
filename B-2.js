a27964188974deb481d451c2ddef7694d001b820
Noor <nkilano@hotmail.com>
2021-09-30 11:35:02 +0200

    Add files via upload

---
+++ B-2.js
@@ -0,0 +1,16 @@
+//Print Odd Numbers 1 - 135
+//Write a program using JavaScript that will print all the odd numbers from 1 to 135.
+
+// pseudo code 
+// first we will create tow variables the first one content the (1) the second one content 
+// the result of adding the numbers then we print the result.
+//diagram  
+// 
+
+//code
+
+for (let x =1; x <= 135; x++) {
+     if (x%2!=0){
+         console. log (x);
+     }
+}
