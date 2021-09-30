bfb5b0f0d2fe332ab397dca227e22ce3be06ae50
Noor <nkilano@hotmail.com>
2021-09-30 16:03:44 +0200

    Update B-3.js

--- B-3.js
+++ B-3.js
@@ -4,9 +4,10 @@
 
 //Number is: 0 Sum: 0, Number is: 1 Sum: 1, Number is: 2 Sum: 3, Number is: 3 Sum: 6
 // pseudo code 
-// 1- make a variable with value 0
-// 2- for loop to to count the sum 
-// 3- sum is + so the two variable  
+//1- make a variable sum with value 0
+// 2- for loop to count the numbers then add one to increase until 135
+// 3- sum is the number +  sum so the two variable 
+// 4- print the result  
 //diagram  
 // 
 
